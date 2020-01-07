7244c9a1f46f9e19eba654921abe28a2a7d78f28
luoxuetong <luoxuetong@9fbank.com.cn>
2020-01-07 18:16:20 +0800

    初始版本提交

---
+++ src/test/java/com/allen/spring/eureka/server/AllenEurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allen.spring.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllenEurakaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
