e4737678babbc2ebffd7af7ea006b226f651f55b
allen <allen@localhost>
2020-05-12 23:54:45 +0800

    修改security

---
+++ src/main/java/com/allen/spring/eureka/server/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.allen.spring.eureka.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * security配置
+ *
+ * @author Allen
+ * @date 2020年5月12日
+ * @since 1.0.0
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// Spring Security 默认开启了所有 CSRF 攻击防御，需要禁用 /eureka 的防御
+		http.csrf().ignoringAntMatchers("/eureka/**");
+		// 访问eureka控制台和/actuator时能做安全控制
+		super.configure(http);
+//        http.csrf().disable();//禁用CSRF
+//        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+}
